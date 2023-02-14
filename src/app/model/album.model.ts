51e03fe349442ee9f2afa377d92086754f47eb60
Julian Torregrosa <jdtorregrosas@gmail.com>
2023-02-14 10:10:00 +0100

    Fix models

--- src/app/model/album.model.ts
+++ src/app/model/album.model.ts
@@ -1,16 +1,5 @@
 export interface Album {
-  id: string;
-  name: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-  },
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-  }
+  userId: number;
+  id: number;
+  title: string;
 };
--- src/app/model/photo.model.ts
+++ src/app/model/photo.model.ts
@@ -1,16 +1,7 @@
 export interface Photo {
-  id: string;
-  name: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-  },
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-  }
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
 };
