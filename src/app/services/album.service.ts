fa159046f20d03be9493b45c47dd3f2d947c4cf4
Julian Torregrosa <jdtorregrosas@gmail.com>
2023-02-14 10:15:02 +0100

    Add userId as query param in albums service

--- src/app/services/album.service.ts
+++ src/app/services/album.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -12,7 +12,8 @@ export class AlbumService {
   private albumsPath = '/albums'
   constructor(private http: HttpClient) { }
 
-  getAlbums(): Observable<Album[]> {
-    return this.http.get<Album[]>(`${environment.apiBaseUrl}${this.albumsPath}`);
+  getAlbumsByUserId(userId: number): Observable<Album[]> {
+    const params = new HttpParams().set('userId', userId);
+    return this.http.get<Album[]>(`${environment.apiBaseUrl}${this.albumsPath}`, { params });
   }
 }
