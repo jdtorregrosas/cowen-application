d4e10ab1229fc357267574fdf1abd0984361f006
Julian Torregrosa <jdtorregrosas@gmail.com>
2023-02-14 09:49:52 +0100

    Extract user model into a new file

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { User } from 'src/app/services/user.service';
+import { User } from 'src/app/model/user.model';
 
 @Component({
   selector: 'app-user-card',
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,32 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-
-export interface User {
-  id: string;
-  name: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-  },
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-  }
-};
+import { User } from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
   usersPath = '/users'
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<any>(`${environment.apiBaseUrl}${this.usersPath}`);
+    return this.http.get<User[]>(`${environment.apiBaseUrl}${this.usersPath}`);
   }
 }
